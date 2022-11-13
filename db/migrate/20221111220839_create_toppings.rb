e741ae968048aa7c4bec854aae5c28feab10d5dd
Mike Shippy <mikeship04@gmail.com>
2022-11-13 16:15:41 -0700

    test

--- db/migrate/20221111220839_create_toppings.rb
+++ db/migrate/20221111220839_create_toppings.rb
@@ -1,6 +1,6 @@
 class CreateToppings < ActiveRecord::Migration[7.0]
   def change
-    table_exists?(:toppings) ? nil : create_table :toppings do |t|
+    create_table(:toppings, if_not_exists: true) do |t|
       t.string :name
 
       t.timestamps
--- db/migrate/20221111232633_pizzas_toppings.rb
+++ db/migrate/20221111232633_pizzas_toppings.rb
@@ -1,6 +1,6 @@
 class PizzasToppings < ActiveRecord::Migration[7.0]
   def change
-    table_exists?(:pizzas_toppings) ? nil : create_table :pizzas_toppings, id: false do |t|
+    create_table(:pizzas_toppings, if_not_exists: true) id: false do |t|
       t.belongs_to :pizza
       t.belongs_to :topping
     end
